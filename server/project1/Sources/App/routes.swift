ed6e0171c1430d44851f54468babfa6a6384c4c8
Matthew Folbigg <matthew.folbigg@icloud.com>
2022-07-18 15:04:40 +0100

    Completed Project 1

---
+++ server/project1/Sources/App/routes.swift
@@ -0,0 +1,54 @@
+import Foundation
+import Leaf
+import Fluent
+import Vapor
+
+func routes(_ app: Application) throws {
+    app.get { req in
+        return req.view.render("index", ["title": "Hello Vapor!"])
+    }
+    
+    app.get { req -> EventLoopFuture<View> in
+        let context = [String: String]()
+        return req.view.render("home", context)
+    }
+    
+    
+    
+    app.get("staff", ":name") { req -> EventLoopFuture<View> in
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.internalServerError)
+        }
+        
+        struct StaffView: Codable {
+            var name: String?
+            var bio: String?
+            var allNames: [String]
+        }
+        
+        let leafContext: StaffView
+        
+        let bios = [
+            "kirk": "My name is James Kirk and I love snakes.",
+            "picard": "My name is Jean-Luc Picard and I'm mad for fish.",
+            "sisko": "My name is Benjamin Sisko and I'm all about the budgies.",
+            "janeway": "My name is Kathryn Janeway and I want to hug every hamster.",
+            "archer": "My name is Jonathan Archer and beagles are my thing."
+        ]
+        
+        if let bio = bios[name] {
+            leafContext = StaffView(name: name, bio: bio, allNames: bios.keys.sorted())
+        } else {
+            leafContext = StaffView(allNames: bios.keys.sorted())
+        }
+        
+        return req.view.render("staff", leafContext)
+    }
+    
+    app.get("contact") { req -> EventLoopFuture<View> in
+        let context = ["": ""]
+        return req.view.render("contact", context)
+    }
+    
+    try app.register(collection: TodoController())
+}
